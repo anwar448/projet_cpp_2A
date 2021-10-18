373b75c0e7bae4e61c2219e54c9959eb679fffe3
ahmedramijouini <ahmedrami.jouini1@esprit.tn>
2021-10-18 11:35:02 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QFile>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    // loading StyleSheetFile.
+        QFile styleSheetFile(":/StyleSheet/Dtor.qss");
+        styleSheetFile.open(QFile::ReadOnly);
+        QString styleSheet = QLatin1String(styleSheetFile.readAll());
+        a.setStyleSheet(styleSheet);
+    w.show();
+    return a.exec();
+
+}
---
+++ mainwindow.cpp
@@ -0,0 +1,60 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->stackedWidget->setCurrentIndex(0);
+    QPixmap pix("C:/Users/rami/Downloads/background.png");
+        ui->photo->setPixmap(pix);
+        QPixmap pix1("C:/Users/rami/Downloads/recipe.png");
+            ui->facture ->setPixmap(pix1);
+            QPixmap pix2("C:/Users/rami/Downloads/chariot.png");
+                ui->fournisseurs ->setPixmap(pix2);
+                QPixmap pix3("C:/Users/rami/Downloads/ressources humaine.png");
+                    ui->ressources ->setPixmap(pix3);
+                    QPixmap pix4("C:/Users/rami/Downloads/client.png");
+                        ui->client ->setPixmap(pix4);
+                        QPixmap pix5("C:/Users/rami/Downloads/produit.png");
+                            ui->produit ->setPixmap(pix5);
+                            QPixmap pix6("C:/Users/rami/Downloads/service.png");
+                                ui->service ->setPixmap(pix6);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_5_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(3);
+}
+
+void MainWindow::on_pushButton_8_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+void MainWindow::on_pushButton_6_clicked()
+{
+     ui->stackedWidget->setCurrentIndex(1);
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+     ui->stackedWidget->setCurrentIndex(4);
+}
+
+void MainWindow::on_pushButton_9_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
